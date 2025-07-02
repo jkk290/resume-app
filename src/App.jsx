19243220848d7bfb08fdd1739ff3a4272c5402d1
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-02 09:41:04 -1000

    Create EducationInfoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import GeneralInfoForm from './components/GeneralInfoForm'
+import EducationInfoForm from './components/EducationInfoForm'
 
 function App() {
 
   return (
     <>
       <GeneralInfoForm />
+      <br/>
+      <EducationInfoForm />
+      <br/>
     </>
   )
 }
---
+++ src/components/EducationInfoForm.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+function EducationInfoForm() {
+    const [educationInfo, setEducationInfo] = useState({schoolName: '', studyTitle: '', studyDate: ''});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function onChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        setEducationInfo({...educationInfo, [name]:value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsSubmitted(true);
+    }
+
+    function clearState() {
+        setEducationInfo({schoolName: '', studyTitle: '', studyDate: ''});
+        setIsSubmitted(false);
+    }
+
+    function handleEdit() {
+        setIsSubmitted(false);
+    }
+
+    function EditButton() {
+        if (isSubmitted) {
+            return (
+                <button onClick={handleEdit} type='button'>Edit</button>
+            )
+        }
+    }
+
+    function ShowInfo() {
+        if (isSubmitted) {
+            return (
+                <>
+                    <h1>{educationInfo.schoolName}</h1>
+                    <p>{educationInfo.studyTitle}</p>
+                    <p>{educationInfo.studyDate}</p>
+                </>
+            )
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    School Name
+                    <input 
+                    type='text'
+                    name='schoolName'
+                    value={educationInfo.schoolName}
+                    onChange={onChange}
+                    disabled={isSubmitted === true ? true : false}  
+                    />
+                </label>
+                <br />
+                <label>
+                    Title of study
+                    <input
+                    type='text'
+                    name='studyTitle'
+                    value={educationInfo.studyTitle}
+                    onChange={onChange}
+                    disabled={isSubmitted}  
+                    />
+                </label>
+                <br />
+                <label>
+                    Date of study
+                    <input
+                    type='date'
+                    name='studyDate'
+                    value={educationInfo.studyDate}
+                    onChange={onChange}
+                    disabled={isSubmitted}  
+                    />
+                </label>
+                <br />
+                <input type="submit" />
+                <button onClick={clearState} type='reset'>Clear</button>
+                <EditButton />
+            </form>
+            <ShowInfo />
+        </>
+    )
+
+}
+
+export default EducationInfoForm;
