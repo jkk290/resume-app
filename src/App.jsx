9be38f5ee2bc546de2a9995433bbfde0ee7c3266
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-07-01 21:48:56 -1000

    Create GeneralInfoForm component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import GeneralInfoForm from './components/GeneralInfoForm'
+
+function App() {
+
+  return (
+    <>
+      <GeneralInfoForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GeneralInfoForm.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+
+function GeneralInfoForm() {
+    const [generalInfo, setGeneralInfo] = useState({firstName: '', lastName: '', email: '', phoneNum: ''});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function onChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        setGeneralInfo({...generalInfo, [name]:value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsSubmitted(true);
+    }
+
+    function clearState(e) {
+        setGeneralInfo({firstName: '', lastName: '', email: '', phoneNum: ''});
+        setIsSubmitted(false);
+    }
+
+    function handleEdit() {
+        setIsSubmitted(false);
+    }
+
+    function EditButton() {
+        if (isSubmitted) {
+            return (
+                <button onClick={handleEdit} type='button'>Edit</button>
+            )
+        }
+    }
+
+    function ShowInfo() {
+        if (isSubmitted) {
+            return (
+                <>
+                    <h1>{generalInfo.firstName + ' ' + generalInfo.lastName}</h1>
+                    <p>{generalInfo.email}</p>
+                    <p>{generalInfo.phoneNum}</p>
+                </>
+            )
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    First Name
+                    <input 
+                    type='text'
+                    name='firstName'
+                    value={generalInfo.firstName}
+                    onChange={onChange}
+                    disabled={isSubmitted === true ? true : false}  
+                    />
+                </label>
+                <br />
+                <label>
+                    Last Name
+                    <input
+                    type='text'
+                    name='lastName'
+                    value={generalInfo.lastName}
+                    onChange={onChange}
+                    disabled={isSubmitted}  
+                    />
+                </label>
+                <br />
+                <label>
+                    Email
+                    <input
+                    type='email'
+                    name='email'
+                    value={generalInfo.email}
+                    onChange={onChange}
+                    disabled={isSubmitted}  
+                    />
+                </label>
+                <br />
+                <label>
+                    Phone Number
+                    <input
+                    type='tel'
+                    name='phoneNum'
+                    value={generalInfo.phoneNum}
+                    onChange={onChange}
+                    disabled={isSubmitted}  
+                    />
+                </label>
+                <br />
+                <input type="submit" />
+                <button onClick={clearState} type='reset'>Clear</button>
+                <EditButton />
+            </form>
+            <ShowInfo />
+        </>
+    )
+
+}
+
+export default GeneralInfoForm;
